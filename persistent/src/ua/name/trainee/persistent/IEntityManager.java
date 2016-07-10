5d6583d4db6dd5681149b10d822fd5ad22d26a8e
anton <anton@b0rnUA.Dlink>
2016-07-10 15:13:51 +0300

    Added entity exception processing

--- persistent/src/ua/name/trainee/persistent/IEntityManager.java
+++ persistent/src/ua/name/trainee/persistent/IEntityManager.java
@@ -1,11 +1,12 @@
 package ua.name.trainee.persistent;
 
 import ua.org.trainee.entity.IEntity;
+import ua.org.trainee.exception.EntityNotFoundException;
 
 public interface IEntityManager
 {
 	<E extends IEntity<?>> void persist(E entity);
 	<E extends IEntity<?>> void merge(E entity);
-	<I, E extends IEntity<I>> IEntity<I> find(Class<E> clazz, I id);
+	<I, E extends IEntity<I>> IEntity<I> find(Class<E> clazz, I id) throws EntityNotFoundException;
 	<I, E extends IEntity<I>> void remove(Class<E> clazz, I id);
 }
--- persistent/src/ua/name/trainee/persistent/InMemoryEntityManager.java
+++ persistent/src/ua/name/trainee/persistent/InMemoryEntityManager.java
@@ -1,8 +1,8 @@
 package ua.name.trainee.persistent;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -11,22 +11,22 @@ import ua.org.trainee.entity.Customer;
 import ua.org.trainee.entity.IEntity;
 import ua.org.trainee.entity.Location;
 import ua.org.trainee.entity.Workstation;
+import ua.org.trainee.exception.EntityNotFoundException;
 
 public final class InMemoryEntityManager implements IEntityManager
 {
 
 	private static 
-		Map<Class<? extends IEntity<?>>, Set<? extends IEntity<?>>> tables = 
-			new HashMap<>();
-	
+		Map<Class<? extends IEntity<?>>, Set<? extends IEntity<?>>> tables = new HashMap<>();
+
 	static
 	{
-		tables.put(Location.class, ConcurrentHashMap.<Location>newKeySet());
-		tables.put(Workstation.class, ConcurrentHashMap.<Workstation>newKeySet());
-		tables.put(Customer.class, ConcurrentHashMap.<Customer>newKeySet());
-		tables.put(Contract.class, ConcurrentHashMap.<Contract>newKeySet());
+		tables.put(Location.class, ConcurrentHashMap.<Location> newKeySet());
+		tables.put(Workstation.class, ConcurrentHashMap.<Workstation> newKeySet());
+		tables.put(Customer.class, ConcurrentHashMap.<Customer> newKeySet());
+		tables.put(Contract.class, ConcurrentHashMap.<Contract> newKeySet());
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	@Override
 	public <E extends IEntity<?>> void persist(E entity)
@@ -39,17 +39,25 @@ public final class InMemoryEntityManager implements IEntityManager
 	{
 		throw new UnsupportedOperationException();
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	@Override
 	public <I, E extends IEntity<I>> IEntity<I> find(Class<E> clazz, I id)
+			throws EntityNotFoundException
 	{
-		return (IEntity<I>) tables.get(clazz).stream()
-				.filter(e -> e.getIdentifier().equals(id))
-				.findAny()
-				.get();
+		try
+		{
+			return (IEntity<I>) tables.get(clazz).stream()
+					.filter(e -> e.getIdentifier().equals(id))
+					.findAny()
+					.get();
+		} catch (NoSuchElementException e)
+		{
+			throw new EntityNotFoundException(
+					clazz + " with ID " + id + "is not found");
+		}
 	}
-	
+
 	@Override
 	public <I, E extends IEntity<I>> void remove(Class<E> clazz, I id)
 	{

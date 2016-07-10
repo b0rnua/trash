401f8e736750f3d3dd2afe2a29a91e003cac3372
anton <anton@b0rnUA.Dlink>
2016-07-10 15:12:57 +0300

    Added exception module

---
+++ exception/src/ua/org/trainee/exception/EntityNotFoundException.java
@@ -0,0 +1,16 @@
+package ua.org.trainee.exception;
+
+public class EntityNotFoundException extends Exception
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public EntityNotFoundException(String message)
+	{
+		super(message);
+	}
+
+}
